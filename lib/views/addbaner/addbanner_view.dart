9a01cfecf361d7797840b6af4edbe6271d1da75b
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-12-25 11:09:13 +0500

    ali

--- lib/views/addbaner/addbanner_view.dart
+++ lib/views/addbaner/addbanner_view.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
@@ -25,7 +23,7 @@ class _AddBannerState extends State<AddBanner> {
           automaticallyImplyLeading: false,
           forceMaterialTransparency: true,
           title: TitleTopBar(
-            name: 'New home panner',
+            name: 'New home banner',
             ontap: () {
               Get.back();
             },
@@ -34,56 +32,61 @@ class _AddBannerState extends State<AddBanner> {
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: SafeArea(
-              child: Column(
-            children: [
-              AddBannerBox(
-                onPressed: (){controller.selectProfileImage();},
-                text: 'Choose Page Panner',
-                buttonheight: 48.0,
-                fontSize: 20.0,
-                buttonwidth: MediaQuery.of(context).size.width * 0.42,
-                width: MediaQuery.of(context).size.width * 0.94,
-                height: MediaQuery.of(context).size.height * 0.18,
-              ),
-              SizedBox(
-                height: 13,
-              ),
-              Container(
-                padding: const EdgeInsets.all(14.0),
-                width: MediaQuery.of(context).size.width * 0.95,
-                height: MediaQuery.of(context).size.width * 0.4,
-                decoration: BoxDecoration(
-                  color: hintcolor,
-                  borderRadius: BorderRadius.circular(20),
+            child: Column(
+              children: [
+                AddBannerBox(
+                  onPressed: () {
+                    controller.selectProfileImage();
+                  },
+                  text: 'Choose Page Banner',
+                  buttonheight: 48.0,
+                  fontSize: 20.0,
+                  buttonwidth: MediaQuery.of(context).size.width * 0.42,
+                  width: MediaQuery.of(context).size.width * 0.94,
+                  height: MediaQuery.of(context).size.height * 0.18,
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(''),
-                    Column(
+                SizedBox(
+                  height: 13,
+                ),
+                if (controller.bannerImage != null)
+                  Container(
+                    padding: const EdgeInsets.all(14.0),
+                    width: MediaQuery.of(context).size.width * 0.95,
+                    height: MediaQuery.of(context).size.width * 0.4,
+                    decoration: BoxDecoration(
+                      color: hintcolor,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                       Image.file(controller.bannerImage!),
-                        SizedBox(
-                          height: 12,
+                        Text(''),
+                        Column(
+                          children: [
+                            Image.file(controller.bannerImage!,height: 80,width: 80,),
+                            SizedBox(
+                              height: 12,
+                            ),
+                            Text(
+                              controller.bannerImageName!,
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 119, 110, 110),
+                              ),
+                            ),
+                          ],
                         ),
-                        Text(
-                          'Hello, Rounded Container!',
-                          style: TextStyle(
-                              color: const Color.fromARGB(255, 119, 110, 110)),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SvgPicture.asset('assets/images/edit.svg'),
+                          ],
                         ),
                       ],
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SvgPicture.asset('assets/images/edit.svg'),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          )),
+                  ),
+              ],
+            ),
+          ),
         ),
         bottomNavigationBar: Padding(
           padding: const EdgeInsets.all(14),
