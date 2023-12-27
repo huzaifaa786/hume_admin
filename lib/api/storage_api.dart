ddfe745d1d11b349837704f9e1f815ce8c2430f1
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-12-27 15:44:30 +0500

    ali

---
+++ lib/api/storage_api.dart
@@ -0,0 +1,87 @@
+import 'dart:developer';
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart' as storage;
+import 'package:flutter/services.dart';
+import 'package:hume_admin/exceptions/storage_api_exceptions.dart';
+import 'package:hume_admin/helper/data_model.dart';
+
+
+class StorageApi {
+  Future<CloudStorageResult> uploadBannerImage({
+    required String shopId,
+    required File imageToUpload,
+  }) async {
+    final imageFileName = "SBI._$shopId";
+
+    final storage.Reference storageReference = storage.FirebaseStorage.instance
+        .ref()
+        .child("shops/bannerImages/$shopId/$imageFileName");
+
+    try {
+      final storage.UploadTask uploadTask =
+          storageReference.putFile(imageToUpload);
+
+      final storage.TaskSnapshot storageTaskSnapshot =
+          await Future.value(uploadTask);
+
+      final downloadUrl = await storageTaskSnapshot.ref.getDownloadURL();
+
+      if (uploadTask.storage.bucket.isNotEmpty) {
+        final url = downloadUrl.toString();
+        return CloudStorageResult(
+          imageUrl: url,
+          imageFileName: imageFileName,
+        );
+      } else {
+        throw StorageApiException(
+          title: 'Server Error',
+          message: 'An error occured while uploading the image.',
+        );
+      }
+    } on PlatformException catch (e) {
+      throw StorageApiException(
+        title: 'Failed to upload image',
+        message: e.message,
+      );
+    }
+  }
+
+  Future<CloudStorageResult> uploadLogoImage({
+    required String shopId,
+    required File imageToUpload,
+  }) async {
+    final imageFileName = "SLI._$shopId";
+
+    final storage.Reference storageReference = storage.FirebaseStorage.instance
+        .ref()
+        .child("shops/logoImages/$shopId/$imageFileName");
+
+    try {
+      final storage.UploadTask uploadTask =
+          storageReference.putFile(imageToUpload);
+
+      final storage.TaskSnapshot storageTaskSnapshot =
+          await Future.value(uploadTask);
+
+      final downloadUrl = await storageTaskSnapshot.ref.getDownloadURL();
+
+      if (uploadTask.storage.bucket.isNotEmpty) {
+        final url = downloadUrl.toString();
+        return CloudStorageResult(
+          imageUrl: url,
+          imageFileName: imageFileName,
+        );
+      } else {
+        throw StorageApiException(
+          title: 'Server Error',
+          message: 'An error occured while uploading the image.',
+        );
+      }
+    } on PlatformException catch (e) {
+      throw StorageApiException(
+        title: 'Failed to upload image',
+        message: e.message,
+      );
+    }
+  }
+}
