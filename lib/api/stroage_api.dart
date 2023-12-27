cb13fc495af8a59284aaf69a4af9b61118a16b48
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-12-27 15:37:22 +0500

    ali

--- lib/api/stroage_api.dart
+++ lib/api/stroage_api.dart
@@ -1,4 +1,3 @@
-
 import 'dart:developer';
 import 'dart:io';
 import 'dart:math';
@@ -6,7 +5,6 @@ import 'package:firebase_storage/firebase_storage.dart' as storage;
 import 'package:flutter/services.dart';
 import 'package:hume_admin/exceotions/stroage_api.dart';
 import 'package:hume_admin/helper/data_model.dart';
-
 
 class StorageApi {
   Future<CloudStorageResult> uploadProfileImage({
@@ -40,6 +38,84 @@ class StorageApi {
         throw StorageApiException(
           title: 'Server Error',
           message: 'An error occurred while uploading the image.',
+        );
+      }
+    } on PlatformException catch (e) {
+      throw StorageApiException(
+        title: 'Failed to upload image',
+        message: e.message,
+      );
+    }
+  }
+
+  Future<CloudStorageResult> uploadBannerImage({
+    required String shopId,
+    required File imageToUpload,
+  }) async {
+    final imageFileName = "SBI._$shopId";
+
+    final storage.Reference storageReference = storage.FirebaseStorage.instance
+        .ref()
+        .child("shops/bannerImages/$shopId/$imageFileName");
+
+    try {
+      final storage.UploadTask uploadTask =
+          storageReference.putFile(imageToUpload);
+
+      final storage.TaskSnapshot storageTaskSnapshot =
+          await Future.value(uploadTask);
+
+      final downloadUrl = await storageTaskSnapshot.ref.getDownloadURL();
+
+      if (uploadTask.storage.bucket.isNotEmpty) {
+        final url = downloadUrl.toString();
+        return CloudStorageResult(
+          imageUrl: url,
+          imageFileName: imageFileName,
+        );
+      } else {
+        throw StorageApiException(
+          title: 'Server Error',
+          message: 'An error occured while uploading the image.',
+        );
+      }
+    } on PlatformException catch (e) {
+      throw StorageApiException(
+        title: 'Failed to upload image',
+        message: e.message,
+      );
+    }
+  }
+
+  Future<CloudStorageResult> uploadLogoImage({
+    required String shopId,
+    required File imageToUpload,
+  }) async {
+    final imageFileName = "SLI._$shopId";
+
+    final storage.Reference storageReference = storage.FirebaseStorage.instance
+        .ref()
+        .child("shops/logoImages/$shopId/$imageFileName");
+
+    try {
+      final storage.UploadTask uploadTask =
+          storageReference.putFile(imageToUpload);
+
+      final storage.TaskSnapshot storageTaskSnapshot =
+          await Future.value(uploadTask);
+
+      final downloadUrl = await storageTaskSnapshot.ref.getDownloadURL();
+
+      if (uploadTask.storage.bucket.isNotEmpty) {
+        final url = downloadUrl.toString();
+        return CloudStorageResult(
+          imageUrl: url,
+          imageFileName: imageFileName,
+        );
+      } else {
+        throw StorageApiException(
+          title: 'Server Error',
+          message: 'An error occured while uploading the image.',
         );
       }
     } on PlatformException catch (e) {
