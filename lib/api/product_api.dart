7b7a81380ee00dc4ec044c8fdbc050329d889f55
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-12-29 13:07:16 +0500

    ali

--- lib/api/product_api.dart
+++ lib/api/product_api.dart
@@ -3,10 +3,10 @@ import 'package:flutter/services.dart';
 import 'package:hume_admin/exceotions/database_exception.dart';
 import 'package:hume_admin/models/product_model.dart';
 
-
 class ProductApi {
   static final _firestore = FirebaseFirestore.instance;
-  final CollectionReference _productCollection = _firestore.collection("products");
+  final CollectionReference _productCollection =
+      _firestore.collection("products");
 
   Future<void> createProduct(ProductModel product) async {
     try {
@@ -14,6 +14,23 @@ class ProductApi {
     } on PlatformException catch (e) {
       throw DatabaseApiException(
         title: 'Failed to create product',
+        message: e.message,
+      );
+    }
+  }
+
+  Future<List<ProductModel>> getAllProducts() async {
+    try {
+      final QuerySnapshot<Map<String, dynamic>> snapshot =
+          await _productCollection.get() as QuerySnapshot<Map<String, dynamic>>;
+      final List<ProductModel> products = snapshot.docs
+          .map((doc) =>
+              ProductModel.fromJson(doc.data() as Map<String, dynamic>))
+          .toList();
+      return products;
+    } on PlatformException catch (e) {
+      throw DatabaseApiException(
+        title: 'Failed to fetch products',
         message: e.message,
       );
     }
